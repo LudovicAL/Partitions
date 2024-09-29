0c388fffcd9222c52252aeb932d432fd4e4529c7
Ludovic <ludovicaubut@hotmail.com>
2024-09-29 01:21:51 -0400

    Ajout d'une page web

---
+++ assets/app.js
@@ -0,0 +1,63 @@
+//DATA PROCESSING
+requestTitles()
+   .then(response => response.json())
+   .then(data => {
+      if (data.message === "Not Found") {
+         console.log("Data not found");
+         return;
+      }
+      console.log("Data retrieved!\nProcessing data...");
+      let tuneList = document.getElementById("myList");
+      while (tuneList.hasChildNodes()) {
+         tuneList.removeChild(tuneList.firstChild);
+      }
+      for(let i = 0, max = data.tree.length; i < max; i++) {
+         let tunePath = data.tree[i].path;
+         let tuneNames = tunePath.substring(0, tunePath.toLowerCase().indexOf(".pdf")).replace(";", " | ");
+         let linkElement = document.createElement("a");
+         linkElement.title = tuneNames;
+         linkElement.innerHTML = tuneNames;
+         linkElement.href = "https://github.com/LudovicAL/Partitions/blob/main/" + tunePath.replace(" ", "%20").replace(";", "%3B");
+         linkElement.classList.add('link-offset-2');
+         linkElement.classList.add('link-offset-3-hover');
+         linkElement.classList.add('link-underline');
+         linkElement.classList.add('link-underline-opacity-0');
+         linkElement.classList.add('link-underline-opacity-75-hover');
+         let listElement = document.createElement("li");
+         listElement.classList.add('list-group-item');
+         listElement.classList.add('list-group-item-action');
+         listElement.appendChild(linkElement);
+         tuneList.appendChild(listElement);
+      }
+   })
+   
+function requestTitles() {
+   console.log("Retrieving data...");
+   return Promise.resolve(fetch(`https://api.github.com/repos/LudovicAL/Partitions/git/trees/main`));
+}
+//LIST FILTERING
+$(document).ready(function(){
+   $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myList li").filter(function() {
+         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+   });
+});
+//TOP BUTTON SETUP
+let topButton = document.getElementById("topButton");
+window.onscroll = function () {
+   scrollFunction();
+};
+function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topButton.classList.remove("d-none");
+   } else {
+      topButton.classList.add("d-none");
+   }
+}
+topButton.addEventListener("click", backToTop);
+function backToTop() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+}
